1eebe55de3d7885e919001c49ac54ae9ce108360
Vivek <vivek.ratnaparkhi10@gmail.com>
2018-05-05 23:32:19 +0530

    print linkedlist reverse order

---
+++ core_cs/data_structures/04_linked_list/06_print_in_reverse.py
@@ -0,0 +1,24 @@
+"""
+ Print elements of a linked list in reverse order as standard output
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ 
+"""
+
+def ReversePrint(head):
+    if(head is None):
+        return
+    else:
+        data = []
+        while(head is not None):
+            data.append(head.data)
+            head = head.next
+        for item in reversed(data):
+            print(item)
